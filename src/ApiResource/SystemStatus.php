3b0349e38c8712ecafca581858f8731f473894ae
Sinan ŞAHİN <sinansahinwm@gmail.com>
2023-03-21 16:00:49 +0300

    state providers added

--- src/ApiResource/SystemStatus.php
+++ src/ApiResource/SystemStatus.php
@@ -2,25 +2,25 @@
 
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 
 #[ApiResource(
     uriTemplate: "system_status",
     shortName: "Get System Status",
     operations: [
-        new Get(
-            controller: SystemStatus::class
-        )
-    ]
+        new Get()
+    ],
+    provider: SystemStatusProvider::class
 )]
 #[AsController]
-class SystemStatus extends AbstractController
+class SystemStatus
 {
 
-    /**
-     * @var bool $status
-     */
-    private bool $status = FALSE;
+    public $status;
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
 
 }
