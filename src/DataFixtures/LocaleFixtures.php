3f409a47e4ea9e7bdd676ba9e34466a6d5f16cb2
Sinan ŞAHİN <sinansahinwm@gmail.com>
2023-03-23 11:27:36 +0300

    translation hooks added

---
+++ src/DataFixtures/LocaleFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Locale;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class LocaleFixtures extends AppFixtures implements OrderedFixtureInterface
+{
+
+    public function load(ObjectManager $manager): void
+    {
+
+        foreach (self::LOCALES as $locale) {
+            $myLocale = new Locale();
+            $myLocale->setName($locale["name"]);
+            $myLocale->setCode($locale["code"]);
+            $myLocale->setFlag($locale["flag"]);
+            $manager->persist($myLocale);
+        }
+
+        $manager->flush();
+    }
+
+    public function getOrder(): int
+    {
+        return 0;
+    }
+}
---
+++ src/Entity/Locale.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use App\ApiResource\Hook\TranslationGetCollectionHookByName;
+use App\Repository\LocaleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ApiResource(
+    operations: [
+        new GetCollection(
+
+            provider: TranslationGetCollectionHookByName::class
+        )
+
+    ],
+    paginationEnabled: false
+)]
+#[ORM\Entity(repositoryClass: LocaleRepository::class)]
+class Locale
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $code = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $flag = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getFlag(): ?string
+    {
+        return $this->flag;
+    }
+
+    public function setFlag(string $flag): self
+    {
+        $this->flag = $flag;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
