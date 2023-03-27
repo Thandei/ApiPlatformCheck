726329f7ebb56bc1c8163a40557f6563d62b4c89
Sinan ŞAHİN <sinansahinwm@gmail.com>
2023-03-27 17:23:36 +0300

    database translation key file creator added

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,8 +5,10 @@ namespace App\Controller\Admin;
 use App\Repository\UserRepository;
 use Facebook\Facebook;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Flex\Configurator\ContainerConfigurator;
 
 #[Route('/admin', name: 'app_admin_')]
--- src/Controller/Admin/Helpers/DatabaseTranslationController.php
+++ src/Controller/Admin/Helpers/DatabaseTranslationController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller\Admin\Helpers;
 
+use App\ApiResource\Normalizer\TranslatableTextNormalizer;
 use App\Controller\Admin\AdminBaseController;
 use App\Repository\GenusAttributeRepository;
 use App\Repository\LocaleRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Psr\Cache\InvalidArgumentException;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[Route('/admin', name: 'app_admin_')]
@@ -16,10 +20,29 @@ class DatabaseTranslationController extends AdminBaseController
 
     const TRANSLATION_ATTRIBUTES = ["appName", "name"];
     const TRANSLATION_PREFIX = 'app.database.';
+    const REMAKE_DATABASE_TRANSLATIONS_CACHE_TIME = 3600;
 
     #[Route('/database/translation/remake', name: 'database_translation_remake')]
     public function index(): Response
     {
+        $cache = new FilesystemAdapter();
+
+        if (!TranslatableTextNormalizer::ALLOW_REMAKING_DATABASE_TRANSLATIONS) {
+            $this->addFlash('pageNotificationError', 'Unable to remake database translations. Please activate Translatable Text Normalizer!');
+        } else {
+            try {
+                $cache->delete('remakeDatabaseTranslations');
+                $remakeDatabaseTranslations = $cache->get('remakeDatabaseTranslations', function (ItemInterface $item) {
+                    $item->expiresAfter(self::REMAKE_DATABASE_TRANSLATIONS_CACHE_TIME);
+                    return TRUE;
+                });
+                $this->addFlash('pageNotificationSuccess', 'Items fetched from the database by the API for ' . self::REMAKE_DATABASE_TRANSLATIONS_CACHE_TIME . ' seconds will be automatically transferred to the translation file. Important: After this period, no translation file will be automatically created.');
+            } catch (InvalidArgumentException) {
+                $this->addFlash('pageNotificationError', 'Unable to remake database translations.');
+            }
+        }
+
+
         return $this->redirectToRoute('app_admin_dashboard');
     }
 
