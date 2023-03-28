02206e8516f27c55795a04070cfd35e33da0ea2a
Sinan ŞAHİN <sinansahinwm@gmail.com>
2023-03-28 11:57:11 +0300

    redirect backend sidebar menu added

--- src/Controller/Admin/Helpers/DocumentationController.php
+++ src/Controller/Admin/Helpers/DocumentationController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller\Admin\Helpers;
 
+use ApiPlatform\Api\UrlGeneratorInterface;
 use App\Controller\Admin\AdminBaseController;
 use App\Service\GithubDocumentationSyncService;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 #[Route('/admin', name: 'app_admin_')]
 class DocumentationController extends AdminBaseController
@@ -19,6 +22,12 @@ class DocumentationController extends AdminBaseController
         $repositoryName = $this->getParameter('app.admin.documentation.repositoryName');
         $myDocumentation = $githubDocumentationSyncService->getApplicationDocumentation($githubToken, $githubUsername, $repositoryName, $request->get("docpath"));
         return $this->render('admin/documentation/index.html.twig', ["documentation" => $myDocumentation]);
+    }
+
+    #[Route('/backend/api', name: 'backend_api')]
+    public function redirectBackendAPI(UrlGeneratorInterface $urlGenerator, Environment $twig): RedirectResponse
+    {
+        return $this->redirect($twig->getGlobals()["shared"]["appApiBaseURL"]);
     }
 
 }
