af03cd327f857507616ccc5c46495b953466d9ae
Thandei <skyforcs@gmail.com>
2023-03-30 11:43:55 +0300

    conf added for form

--- src/Form/LandingSlideType.php
+++ src/Form/LandingSlideType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\LandingSlide;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +13,9 @@ class LandingSlideType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('textcontent')
-            ->add('tags')
-            ->add('image')
+            ->add('textcontent', TextType::class, ["label"=> "Text Content"])
+            ->add('tags', TextType::class, ["label"=> "Tags"])
+            ->add('image',TextType::class, ["label"=> "İmage"])
         ;
     }
 
