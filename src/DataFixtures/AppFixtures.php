066ade2237c86c0defded53f007f43726b3c8fa7
Thandei <skyforcs@gmail.com>
2023-03-30 13:31:47 +0300

    commit

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -15,14 +15,12 @@ class AppFixtures extends Fixture implements OrderedFixtureInterface
 
     // === USER FIXTURES CONFIGURATION == //
     const USERS_COUNT = 100;
-    const USERS_FAKE_ROLES = ["ROLE_ADMIN", "ROLE_USER", "ROLE_SUPER_ADMIN"];
+    const USERS_FAKE_ROLES = ["ROLE_ADMIN", "ROLE_USER"];
     const USER_FAKE_ADMIN = "sinansahinwm@gmail.com";
     const USER_FAKE_ADMIN_PASSWORD = "321";
     const USER_FAKE_ADMIN_ACCOUNT_NAME = "System Administrator";
     const USER_FAKE_ADMIN_NICK_NAME = "system.admin";
     const USER_DONT_USE_HASH = TRUE;
-    const USER_PROFILE_IMAGE_WIDTH = 500;
-    const USER_PROFILE_IMAGE_HEIGHT = 500;
 
     // === LOCALE FIXTURES CONFIGURATION == //
     const LOCALES = [
